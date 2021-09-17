f7962f24bead0a4e4040c414db17eb4a01918838
SuperRedingBros <redingtommy@gmail.com>
2021-09-17 08:45:10 -0400

    cookietest

--- ekus.js
+++ ekus.js
@@ -1,14 +1,35 @@
 let number = 0;
-load()
+let vupgrades = new Map();
+load();
 function load(){
-  const para = document.createElement("p");
-  const node = document.createTextNode("This is new.");
-  para.appendChild(node);
-  const upgrades = document.getElementById("upgradesection")
-  upgrades.appendChild(para)
-
+  try {
+    vupgrades = localStorage.getitem("upgrades");
+    number = localStorage.getitem("number");
+  } catch (e) {
+    vupgrades.set("Scope",1);
+    vupgrades.set("Scopecount",0);
+  }
 }
+  //const para = document.createElement("p");
+  //const node = document.createTextNode("This is new.");
+  //para.appendChild(node);
+  //upgrades.appendChild(para)
+
+
+
+function upgrades(target,value,price,pricemultiplier){
+  if (number>=(price+(vupgrades.get(target+"count")*pricemultiplier))) {
+    number = number - (price+(vupgrades.get(target+"count")*pricemultiplier));
+    vupgrades.set(target, (vupgrades.get(target) + value));
+    vupgrades.set(target+"count", (vupgrades.get(target+"count") + 1));
+    document.getElementById(target).innerHTML = target + ": " + String(vupgrades.get(target+"count"));
+    localStorage.setitem("upgrades",vupgrades)
+  }
+  document.getElementById("hi").innerHTML = String(number);
+}
+
 function clicktest(){
-  number = number + 1;
+  number = number + vupgrades.get("Scope");
   document.getElementById("hi").innerHTML =  String(number);
+  localStorage.setitem("number",number)
 }
