ba5f7178ac1cf7f01bf043fb82f8b5351b0f6212
SuperRedingBros <redingtommy@gmail.com>
2021-09-18 14:30:43 -0400

    Test

--- ekus.js
+++ ekus.js
@@ -1,35 +1,38 @@
 let number = 0;
 let vupgrades = new Map();
+let list = ["Scope"];
 load();
 function load(){
-  try {
-    vupgrades = localStorage.getitem("upgrades");
-    number = localStorage.getitem("number");
-  } catch (e) {
-    vupgrades.set("Scope",1);
-    vupgrades.set("Scopecount",0);
+  list.forEach(myFunction);
+  function myFunction(value, index, array) {
+    if (localStorage.getItem(value) != null ) {
+      vupgrades.set(value,Number(localStorage.getItem(value)));
+      vupgrades.set(value+"count",Number(vupgrades.get(value+"count")));
+    } else {
+      vupgrades.set(value,1)
+      vupgrades.set(value+"count",0)
+    }
   }
 }
+
   //const para = document.createElement("p");
   //const node = document.createTextNode("This is new.");
   //para.appendChild(node);
   //upgrades.appendChild(para)
-
-
-
 function upgrades(target,value,price,pricemultiplier){
-  if (number>=(price+(vupgrades.get(target+"count")*pricemultiplier))) {
-    number = number - (price+(vupgrades.get(target+"count")*pricemultiplier));
-    vupgrades.set(target, (vupgrades.get(target) + value));
-    vupgrades.set(target+"count", (vupgrades.get(target+"count") + 1));
+  if (number>=(price+Number(vupgrades.get(target+"count")*pricemultiplier))) {
+    number = number - (price+Number(vupgrades.get(target+"count")*pricemultiplier));
+    vupgrades.set(target, Number(vupgrades.get(target) + value));
+    vupgrades.set(target+"count", Number(vupgrades.get(target+"count") + 1));
     document.getElementById(target).innerHTML = target + ": " + String(vupgrades.get(target+"count"));
-    localStorage.setitem("upgrades",vupgrades)
+    localStorage.setItem(target,Number(vupgrades.get(target)));
+    localStorage.setItem(target+"count",Number(vupgrades.get(target+"count") + 1));
   }
   document.getElementById("hi").innerHTML = String(number);
 }
 
 function clicktest(){
-  number = number + vupgrades.get("Scope");
+  number = number + Number(vupgrades.get("Scope"));
   document.getElementById("hi").innerHTML =  String(number);
-  localStorage.setitem("number",number)
+  localStorage.setItem("number",number);
 }
